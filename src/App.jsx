44594856b1d6c256bea3a2173e8748d0dae98104
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-07-21 20:30:10 +0530

    mouse Event

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Intro from './Components/intro';
 import Projects from './Components/Projects';
+import React, { useState } from 'react';
 
 function App() {
+  const [coordinates, setCoordinates] = useState({ pageX: 0, pageY: 0});
+  const [mousevisible, setMouseVisible] = useState(true);
+  const handleMouseMove = (e) =>{
+    setCoordinates({ pageX: e.pageX, pageY: e.pageY});
+
+  }
+
+  const handleMouseOut = () =>{
+    setMouseVisible(false);
+  }
+  const handleMouseEnter = () =>{
+    setMouseVisible(true);
+  }
   return (
-    <>
+    <div onMouseMove={handleMouseMove} onMouseLeave={handleMouseOut}  onMouseEnter={handleMouseEnter}>
+    { mousevisible && <div  className="curser" style={{
+      left: `${coordinates.pageX}px`,
+      top: `${coordinates.pageY}px`,
+    }}></div>}
     <Navbar />
     <Intro fName="Mohamed"
      lName="Fazil" 
@@ -21,7 +39,7 @@ function App() {
     
 
 
-    </>
+    </div>
   );
 }
 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,11 +8,12 @@ import Image1 from '../assets/dog.png'
 export default function Projects(){
     return(
         <div className="Projects">
+            .
             <p className="Project-section">Selected Works</p>
             <div className="Project-container">
-                <Card row="row-reverse" Image={Image1} />
+                <Card row="row" Image={Image1} />
                 <Card row="row" Image={Image1}/>
-                <Card row="row-reverse" Image={Image1} />
+                <Card row="row" Image={Image1} />
                 <Card row="row" Image={Image1}/>
 
                 
