839e8229c0d6e163e000af18a94c3784113d7b69
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-07-27 15:05:21 +0530

    Contact page style

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import "../styles/Contact.css"
+import Connection from "../Components/ConnectionLinks";
 
 
 
 export default function Contact(){
     return(
-        <div className="contact">
+        <>
             <Navbar />
-            <h1>Contact Us</h1>
+            <Connection />
+            <div className="contact">
+           <div className="Form-container">
+
+           </div>
+           
         </div>
+
+        </>
+        
     )
 }
