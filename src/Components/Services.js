dac059c36cdd474ef060613468fbc909a2760960
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-08-04 22:17:54 +0530

    service page add

---
+++ src/Components/Services.js
@@ -0,0 +1,55 @@
+const Service = [
+    {
+        id: 1,
+        Sevice: "Logo Designing",
+        Description: "This is a UX/UI designing service",
+        classNames: "one",
+        bgcolor: "linear-gradient(pink, blue)",
+
+    },
+    {
+        id: 2,
+        Sevice: "UI/UX Designing",
+        Description: "This is a UX/UI designing service",
+        classNames: "two",
+        bgcolor: "linear-gradient(white, pink, yellow)",
+
+    },
+    {
+        id: 3,
+        Sevice: "Web Development",
+        Description: "This is a web development service",
+        classNames: "three",
+        bgcolor: "linear-gradient(red, lightblue)",
+
+    },
+    {
+        id: 4,
+        Sevice: "App Development",
+        Description: "This is a mobile app development service",
+        classNames: "four",
+        bgcolor: "linear-gradient(blue, lightblue)",
+
+    },
+    
+
+    {
+        id: 5,
+        Sevice: "Web hosting",
+        Description: "This is a UX/UI designing service",
+        classNames: "five",
+        bgcolor: "linear-gradient(white, purple)",
+
+    },
+  
+    {
+        id: 6,
+        Sevice: "SEO Markting",
+        Description: "This is a UX/UI designing service",
+        classNames: "six",
+        bgcolor: "linear-gradient(white, pink, yellow)",
+
+    }
+];
+
+export default Service;
--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,15 +1,34 @@
 import Navbar from '../Components/Navbar';
 import "../styles/Services.css";
+import Service from "../Components/Services.js";
 
 
 export default function Services() {
   return (
     <section className='Services'>
-    <Navbar />
-    <h1>Our Services</h1>
- 
+      <Navbar />
+      <h1>Our Services</h1>
+      <div className="services-container">
+        <div className="service-card">
+          <div className="list">
+            {Service.map((item, index) =>(
+              <div key={item.id} className='Services'>
+                <p className={item.classNames} style={{ background: item.bgcolor, backgroundClip: "text"}}>{item.Sevice}</p>
+                <p className='Description'>{item.Description}</p>
+              </div>
+            ))}
+          </div>
+         
 
-    
+
+
+
+
+        </div>
+      </div>
+
+
+
 
 
     </section>
