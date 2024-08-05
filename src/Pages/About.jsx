24ea7b7bc69059c03d7d8e044ad8bf38f940ff8b
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-08-05 22:36:50 +0530

    About page started

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,13 +6,24 @@ import Navbar from "../Components/Navbar";
 export default function About() {
     return (
         <>
-        <Navbar />
-        <div className="About-container">
-            <h1>About Me</h1>
+            <Navbar />
+            <div className="About-container">
+                <b>About Me</b>
+                <p>I'm a Web developer |<span> App developer</span></p>
+                {/* <p>
+                    Hi Guys! I am Mohamed Fazil, a web and app developer with a passion for creating
+                    user-friendly, innovative designs. Proficient in HTML, CSS, JavaScript, and Python,
+                    I specialize in robust solutions. My skills also include React Native for mobile
+                    development. Throughout my IT studies, I've actively participated in hackathons,
+                    showcasing my commitment to blending creativity and functionality in every project.
 
-           
-        </div>
-        
-               </>
+
+                </p> */}
+
+
+            </div>
+            
+
+        </>
     )
 }
